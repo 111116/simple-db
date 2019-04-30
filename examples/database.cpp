ad84f22918d5d99e7669502a156b777a837ee745
InamiKuugo <49970790+InamiKuugo@users.noreply.github.com>
2019-05-01 00:55:42 +0800

    Some fixes

---
+++ examples/database.cpp
@@ -0,0 +1,61 @@
+#include "database.h"
+
+/**
+ * Destructs this database.
+*/
+Database::~Database()
+{
+	for (auto p: table)
+		delete(p.second);
+}
+
+/**
+ * Creates a table in this database.
+ * 
+ * @param Name of the table to be created
+ * @param Columns with data types and constraints
+*/
+void Database::create(std::string tableName, std::string traits)
+{
+	table[tableName] = new Table(traits);
+}
+
+/**
+ * Drops a table named ${tableName} from this database.
+ * 
+ * @param Name of the table to be dropped
+*/
+void Database::drop(std::string tableName)
+{
+	table.erase(tableName);
+}
+
+void Database::show()
+{
+	std::cout << "Table" << std::endl;
+	for (auto p: table)
+		std::cout << p.first << std::endl;
+}
+
+/**
+ * Lists columns of a table specified in this database.
+ * 
+ * @param Name of the table
+*/
+void Database::show(std::string tableName)
+{
+	table[tableName]->show();
+}
+
+/**
+ * Finds a table in this database
+ * 
+ * @param Name of the table
+ * @return Pointer to specified table (NULL if not found)
+*/
+Table* Database::operator[](std::string tableName)
+{
+	if (table.count(tableName))
+		return table[tableName];
+	return nullptr;
+}
