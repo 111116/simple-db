d49db74e645d58a326e254d73421a6ed4f8debf6
bilibili <fenggaobuke@gmail.com>
2019-05-03 11:44:00 +0800

    implement Entry move assignment

--- src/entry.h
+++ src/entry.h
@@ -10,6 +10,15 @@ public:
 	using std::vector<data_t*>::vector;
 	Entry(const Entry&) = delete;
 	Entry(Entry&& e): std::vector<data_t*>(e) {}
+	Entry& operator= (Entry&& e)
+	{
+		for (auto p: *this)
+			if (p != nullptr)
+				delete p;
+		std::vector<data_t*>::operator= (static_cast<std::vector<data_t*>>(e));
+		e.clear();
+		return *this;
+	}
 	~Entry()
 	{
 		for (auto p: *this)
