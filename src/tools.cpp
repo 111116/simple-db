15da5526c2a365bc1a1c3df73694ff33f433054e
Chrogeek <15846491+Chrogeek@users.noreply.github.com>
2019-05-05 21:43:32 +0800

    More Chinese comments

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,18 +1,14 @@
 #include "tools.h"
 
-/**
- * Splits a raw MySQL statement into tokens.
- *
- * @param raw Raw MySQL statement.
- * @return A vector containing its tokens.
-*/
+// 对 MySQL 语句进行分词
+// 分词后的 tokens 放进一个 std::vector<std::string> 中后返回
 std::vector<std::string> split(std::string raw)
 {
 	raw.push_back('\n');
 	std::vector<std::string> res;
 	std::string current;
 	bool inString = false;
-	char stringDelimiter; // surrounding delimiter, if inside a string literal: single/double quotation mark
+	char stringDelimiter; // 处理到字符串字面量中间时，该 stringDelimiter 变量保存所用的包围字符串的符号，为单引号或双引号
 
 	auto split = [&]()
 	{
@@ -27,7 +23,7 @@ std::vector<std::string> split(std::string raw)
 		if (inString)
 		{
 			if (raw[i] == '\n')
-				throw "string literal missing terminating character";
+				throw "String literal missing terminating character";
 			current += raw[i];
 			if (raw[i] == stringDelimiter)
 			{
@@ -36,7 +32,7 @@ std::vector<std::string> split(std::string raw)
 					current += stringDelimiter;
 					i++;
 				}
-				else // end of a string literal
+				else // 字符串字面量结束
 				{
 					inString = false;
 				}
@@ -48,14 +44,13 @@ std::vector<std::string> split(std::string raw)
 			}
 			continue;
 		}
-		// outside a string literal
-		// split at whitespace character
+		// 不在字符串字面量中；在空白字符处分词
 		if (raw[i] == ' ' || raw[i] == '\t' || raw[i] == '\n')
 		{
 			split();
 			continue;
 		}
-		// if string literal starts
+		// 字符串字面量起始判断
 		if (raw[i] == '\'' || raw[i] == '\"')
 		{
 			split();
@@ -64,26 +59,22 @@ std::vector<std::string> split(std::string raw)
 			current = raw[i];
 			continue;
 		}
-		// accept "[-_0-9A-Za-z\.]+"
+		// 减号（负号，连字符）、数字、大小写字母、下划线、小数点的连续组合被认为是一整个 token（可能是数字或标识符的名称）
+		// "[-_0-9A-Za-z\.]+"
 		if (isdigit(raw[i]) || isalpha(raw[i]) || raw[i] == '_' ||
 			raw[i] == '-' || raw[i] == '.')
 		{
 			current += raw[i];
 			continue;
 		}
-		// other spliting chars
+		// 其它特殊字符，分词
 		split();
 		res.push_back({raw[i]});
 	}
 	return res;
 }
 
-/**
- * Converts a string to lowercase.
- *
- * @param str String to convert
- * @return Converted lowercase string
-*/
+// 将字符串 str 中的大写字母转换为小写，并返回转换后的字符串
 std::string stringToLower(std::string str)
 {
 	std::transform(str.begin(), str.end(), str.begin(), ::tolower);
--- src/tools.h
+++ src/tools.h
@@ -4,10 +4,8 @@
 #include <vector>
 #include <algorithm>
 
-typedef std::vector<std::string> tokens;
-typedef std::vector<std::string> attrs;
+typedef std::vector<std::string> tokens; // 保存被分词的 MySQL 语句，包含原 MySQL 语句中的每个 token
+typedef std::vector<std::string> attrs; // 保存若干个列的名称。虽然与 tokens 本质相同，但在程序中的语义不同
 
-// split a string
-std::vector<std::string> split(std::string);
-
-std::string stringToLower(std::string);
+std::vector<std::string> split(std::string raw); // 对语句进行分词
+std::string stringToLower(std::string str); // 将字符串中所有大写字母转换为小写
